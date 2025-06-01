220fae0a1ec3e7e7a0465f840ac7e17890d3a433
Leandro <pnunesleandro@gmail.com>
2025-06-01 20:52:35 -0300

    Changed the billList to a FlatList

--- src/components/list/BillList.tsx
+++ src/components/list/BillList.tsx
@@ -1,7 +1,6 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import React from 'react';
 import { data } from '../../../mocks/data';
-import FormButton from '../form/FormButton';
 
 export default function BillList() {
   return (
@@ -9,7 +8,6 @@ export default function BillList() {
       <Text className='text-left font-semibold text-xl mb-4'>
         Contas a Pagar
       </Text>
-
       <View className='flex flex-row mb-6'>
         <Text className='text-left font-semibold text-base basis-3/5'>
           Titulo
@@ -21,28 +19,29 @@ export default function BillList() {
           Valor
         </Text>
       </View>
-      {data.map((bill, index) => (
-        <View
-          key={index}
-          className='flex flex-row mb-3 border-slate-300 border-b h-[3rem]'
-        >
-          <View className='basis-3/5 self-center'>
-            <Text className='text-left font-semibold text-base'>
-              {bill.title}
+
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View className='flex flex-row mb-3 border-slate-300 border-b h-[3rem]'>
+            <View className='basis-3/5 self-center'>
+              <Text className='text-left font-semibold text-base'>
+                {item.title}
+              </Text>
+              <Text className='text-xs text-gray-500'>
+                Essa é uma descrição teste
+              </Text>
+            </View>
+
+            <Text className='text-center font-semibold text-base basis-1/5 self-center'>
+              {item.due_date.slice(0, 5)}
             </Text>
-            <Text className='text-xs text-gray-500'>
-              Essa é uma descrição teste
+            <Text className='text-center font-semibold text-base basis-1/5 self-center'>
+              R$ {item.value}
             </Text>
           </View>
-
-          <Text className='text-center font-semibold text-base basis-1/5 self-center'>
-            {bill.due_date.slice(0, 5)}
-          </Text>
-          <Text className='text-center font-semibold text-base basis-1/5 self-center'>
-            R$ {bill.value}
-          </Text>
-        </View>
-      ))}
+        )}
+      />
     </View>
   );
 }
