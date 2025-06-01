8769eea51d4ac7f08582b60ffbf447c698f66132
Leandro <pnunesleandro@gmail.com>
2025-06-01 18:26:23 -0300

    created a billList component

---
+++ src/components/list/BillList.tsx
@@ -0,0 +1,49 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { data } from '../../../mocks/data';
+import FormButton from '../form/FormButton';
+
+export default function BillList() {
+  return (
+    <View className='m-4'>
+      <Text className='text-left font-semibold text-xl mb-4'>
+        Contas a Pagar
+      </Text>
+
+      <View className='flex flex-row mb-6'>
+        <Text className='text-left font-semibold text-base basis-4/6'>
+          Titulo
+        </Text>
+        <Text className='text-left font-semibold text-base basis-1/6'>
+          Data
+        </Text>
+        <Text className='text-left font-semibold text-base basis-1/6'>
+          Valor
+        </Text>
+      </View>
+      {data.map((bill, index) => (
+        <View
+          key={index}
+          className='flex flex-row mb-3 border-slate-300 border-b h-[3rem]'
+        >
+          <View className='basis-4/6 self-center'>
+            <Text className='text-left font-semibold text-base'>
+              {bill.title}
+            </Text>
+            <Text className='text-sm text-gray-500'>
+              Essa é uma descrição teste
+            </Text>
+          </View>
+
+          <Text className='text-left font-semibold text-base basis-1/6 self-center'>
+            {bill.due_date.slice(0, 5)}
+          </Text>
+          <Text className='text-left font-semibold text-base basis-1/6 self-center'>
+            {bill.value}
+          </Text>
+        </View>
+      ))}
+      <FormButton title='NOVA CONTA' />
+    </View>
+  );
+}
